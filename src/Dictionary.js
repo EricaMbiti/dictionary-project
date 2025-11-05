260cfaef879d8afa70f0fa2454bb09458d8c79fd
EricaMbiti <mbitierica@gmail.com>
2025-11-05 16:46:12 +0200

    Added Dictionary Api intergration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,19 +1,34 @@
 import React,{useState} from "react";
+import axios from "axios";
 import './Dictionary.css';
+
+
+
 export default function Dictionary(){
 
 let [keyword,setKeyword]=useState("");//using states because input has got to be updated 
 
-function handleKeywordChange(event){
 
-setKeyword(event.target.value);//will update the state(keeping track of the word searched)
+function handleResponse(response){
+console.log(response.data);
 
 }
 
 
+
+function handleKeywordChange(event){
+setKeyword(event.target.value);//will update the state(keeping track of the word searched)
+
+}
+
  function search (event) {  //event for when a user enters the info 
 event.preventDefault();
-alert(`Searching for ${keyword} definition`)
+
+//Api intergration
+let apiKey="5t2d0f5d9d5f13f50593407dab36oc25";
+let apiUrl=`https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+axios.get(apiUrl).then(handleResponse);
+
 
  } 
 
